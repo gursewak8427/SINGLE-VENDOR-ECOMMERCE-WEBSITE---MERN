01e0d1222c673fb24c68074edf3e316c478358ba
gursewak8427 <gursewaksaggu2043@gmail.com>
2022-12-15 00:00:52 +0530

    Updated Nodejs URL onrender.com

--- src/sources/keys.js
+++ src/sources/keys.js
@@ -1,7 +1,8 @@
 // NOTE: please don't add Slash ( / )  after URL 
 
 const KEYS = { 
-    NODE_URL: 'http://localhost:8084'
+    // NODE_URL: 'http://localhost:8084'
+    NODE_URL: 'https://ecommerce-backend-fffe.onrender.com' // latest
     // NODE_URL: ' https://stylefactory-backend.herokuapp.com' // latest
 }
 
